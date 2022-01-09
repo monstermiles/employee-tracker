831c98a86526c5a11dcea2e35b43394708207791
monstermiles <miles.emmitt.moss@gmail.com>
2022-01-09 13:58:23 -0600

    made the first inquirer prompt and the function that shows the departments table, got it console logging the data from the departments table.

--- index.js
+++ index.js
@@ -5,12 +5,43 @@ require('dotenv').config()
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // MySQL username,
-      user: process.env.DB_USER,
-      // MySQL password
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+        host: 'localhost',
+        // MySQL username,
+        user: process.env.DB_USER,
+        // MySQL password
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     },
     console.log(`Connected to the classlist_db database.`)
-  );
+);
+
+
+function startPrompts() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'start',
+                message: 'Chose an option.',
+                choices: ['View departments list']
+            }
+        ])
+        .then(data => {
+            if (data.start === 'View departments list') {
+                viewDepartmentList()
+            }
+        })
+}
+
+function viewDepartmentList() {
+    db.query('SELECT * FROM departments', function (err, results) {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log(results)
+        }
+    })
+}
+
+
+startPrompts();
